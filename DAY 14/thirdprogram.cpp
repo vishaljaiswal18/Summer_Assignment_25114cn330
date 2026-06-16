f67643e9d89059fa8f6b934443a125005616c42a
VISHAL  JAISWAL <vishaljaiswalj22@gmail.com>
2026-06-16 22:02:17 +0530

    Added Day 14 programs

---
+++ DAY 14/thirdprogram.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int largest = arr[0], second = -1;
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    cout << "Second Largest = " << second;
+
+    return 0;
+}
